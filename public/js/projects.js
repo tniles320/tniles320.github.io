ca53e59472f819c03b22bf6b82844776195cce82
Taylor Niles <tniles320@gmail.com>
2020-11-24 19:45:48 -0800

    updating projects pages and utilizing css grids to make page more responsive

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,12 +1,12 @@
 if(document.getElementById("healthapp-link") !== null){
     document.getElementById("healthapp-link").addEventListener("click", function() {
-        window.open("https://tniles320.github.io/healthapp", "_blank");
+        window.open("https://github.com/tniles320/healthapp", "_blank");
     });
 };
 
 if(document.getElementById("weather-dashboard-link") !== null){
     document.getElementById("weather-dashboard-link").addEventListener("click", function() {
-        window.open("https://tniles320.github.io/weather-dashboard", "_blank");
+        window.open("https://github.com/tniles320/weather-dashboard", "_blank");
     });
 };
 
@@ -18,6 +18,25 @@ if(document.getElementById("day-scheduler-link") !== null){
 
 if(document.getElementById("bindrr-link") !== null){
     document.getElementById("bindrr-link").addEventListener("click", function() {
-        window.open("https://lit-wildwood-97501.herokuapp.com/", "_blank");
+        window.open("https://github.com/tniles320/bindrr", "_blank");
     });
 };
+
+(function($) {
+    const $window = $(window),
+        $text = $('.web-text');
+
+    function resize() {
+        if ($window.width() < 576) {
+            return $text.removeClass('col-5');
+        }
+    }
+    function resize() {
+        if ($window.width() > 576) {
+            return $text.addClass('col-5');
+        }
+    }
+    $window
+        .resize(resize)
+        .trigger('resize');
+})(jQuery);
