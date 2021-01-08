fc6a89da1d828410893e9c876adfb2c588e65482
Taylor Niles <tniles320@gmail.com>
2021-01-08 12:46:14 -0800

    updating styling and compressing pictures

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const compression = require("compression");
 
 const routes = require("./routes");
 const app = express();
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
